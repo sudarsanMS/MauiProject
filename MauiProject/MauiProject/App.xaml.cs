ec56039ee032c89f03b94e0f186ffb6b115897cc
Sudarsan Muthuselvan <sudarsan.m@syncfusion.com>
2023-04-06 17:36:00 +0530

    MauiProject

---
+++ MauiProject/MauiProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiProject;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiProject/MauiProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiProject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
